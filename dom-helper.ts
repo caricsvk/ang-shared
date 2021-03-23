8e528cf3f8623e944774c4c7b02102e7edfb546b
Richard Casar <caricsvk@gmail.com>
2021-03-23 15:26:08 +0100

    add scrollbar width and addStyleRules

--- dom-helper.ts
+++ dom-helper.ts
@@ -1,6 +1,9 @@
 import { Observable, Subject } from 'rxjs';
 
 export class DomHelper {
+
+  private static scrollbarWidth: number;
+
   static scrollTo(element: HTMLElement, offset: number, duration: number): Observable<unknown> {
     const start = element.scrollTop;
     const change = offset - start;
@@ -30,6 +33,39 @@ export class DomHelper {
     const bottom = rect.bottom <= (window.innerHeight || document.documentElement.clientHeight);
     const right = rect.right <= (window.innerWidth || document.documentElement.clientWidth);
     return fully ? top && left && bottom && right : top && left;
+  }
+
+  static getScrollbarWidth(): number {
+    if (this.scrollbarWidth === undefined) {
+      // Creating invisible container
+      const outer = document.createElement('div');
+      outer.style.visibility = 'hidden';
+      outer.style.overflow = 'scroll'; // forcing scrollbar to appear
+      // @ts-ignore
+      outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+      document.body.appendChild(outer);
+
+      // Creating inner element and placing it in the container
+      const inner = document.createElement('div');
+      outer.appendChild(inner);
+
+      // Calculating difference between container's full width and the child width
+      this.scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
+
+      // Removing temporary elements from the DOM
+      outer.parentNode.removeChild(outer);
+
+    }
+    return this.scrollbarWidth;
+  }
+
+  static addStylesheetRules (...rules: string[]) {
+    var styleEl = document.createElement('style');
+    document.head.appendChild(styleEl);
+    var styleSheet = styleEl.sheet;
+    for (var i = 0; i < rules.length; i++) {
+      styleSheet.insertRule(rules[i], styleSheet.cssRules.length);
+    }
   }
 
   private static easeInOutQuad(t: number, b: number, c: number, d: number): number {
