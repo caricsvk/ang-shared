41a13d292beec224b0b2b8489a008f8c60918f51
Richard Casar <caricsvk@gmail.com>
2022-02-10 18:43:11 +0100

    improve entity service + getAll observable

--- entity.service.ts
+++ entity.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, ReplaySubject, Subject, tap } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -8,12 +8,22 @@ import { map } from 'rxjs/operators';
 })
 export abstract class EntityService<T> {
 
+  protected allEntitiesSubject: Subject<T[]>;
+
   constructor(
     protected httpClient: HttpClient
   ) { }
 
   protected abstract getApiPath(): string;
   protected abstract createT(obj?: object): T;
+
+  getAll() {
+    if (! this.allEntitiesSubject) {
+      this.allEntitiesSubject = new ReplaySubject<T[]>(1);
+    }
+    this.reFetchAll();
+    return this.allEntitiesSubject.asObservable();
+  }
 
   fetchCountSearch(params = new HttpParams()): Observable<number> {
     return this.httpClient.get<{value: number}>(this.getApiPath() + '/count', {params})
@@ -27,15 +37,24 @@ export abstract class EntityService<T> {
 
   save(entity: T): Observable<T> {
     // @ts-ignore
-    return this.httpClient[entity.id ? 'put' : 'post']<any>(this.getApiPath(), entity).pipe(map(obj => this.createT(obj)));
+    return this.httpClient[entity.id ? 'put' : 'post']<any>(this.getApiPath(), entity).pipe(
+      map(obj => this.createT(obj)),
+      tap(() => this.reFetchAll())
+    );
   }
 
   remove(id: string | number) {
-    return this.httpClient.delete<any>(this.getApiPath() + `/${id}`);
+    return this.httpClient.delete<any>(this.getApiPath() + `/${id}`).pipe(tap(() => this.reFetchAll()));
   }
 
   find(id: string | number) {
     return this.httpClient.get<any>(this.getApiPath() + `/${id}`).pipe(map(obj => this.createT(obj)),);
+  }
+
+  protected reFetchAll() {
+    if (this.allEntitiesSubject) {
+      this.fetchSearch().subscribe(entities => this.allEntitiesSubject.next(entities));
+    }
   }
 
 }
