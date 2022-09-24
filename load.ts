789557b582b46c0516f5a1fb292ebb5aef11b147
Richard Casar <caricsvk@gmail.com>
2022-09-24 11:23:05 -0700

    refactor load.ts

--- load.ts
+++ load.ts
@@ -1,68 +1,76 @@
 export class Load {
 
   private static loadedJs: string[] = [];
-  private static depQueue: {[key: string]: string[]} = {};
-  private static callback: {[key: string]: Function} = {};
-  private static inProgress: string[] = [];
+  private static dependentFiles: DependentFile[] = [];
+  private static inProgressFiles: {[key: string]: DependentFile[]} = {};
 
-  public static loadJsOnce(src: string, callback?: () => any, dependencies: string[] = []) {
-    //store callbacks
-    callback = callback || function () {};
-    const oldCallback = typeof Load.callback[src] === "function" ? Load.callback[src] : function () {};
-    (function (oldCallback, callback) {
-      Load.callback[src] = function () {
-        oldCallback();
-        callback();
-      };
-    } (oldCallback, callback));
+  public static loadJsOnce(file: DependentFile | string, callback: Function = () => {}) {
+    if (typeof file === 'string') {
+      file = new DependentFile(file);
+    }
+    const alreadyInDepQueue = this.dependentFiles.includes(file);
+    const inProgressFile = this.inProgressFiles[file.src];
 
-    if (Load.inProgress.indexOf(src) >= 0)
-      return;
-    if (Load.loadedJs.indexOf(src) >= 0) { //script is loaded
-        callback();
-    } else if (this.hasLoadedDependencies(dependencies)) {
-
-      const script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.src = src;
-      script.onload = () => this.loadCallback(src);
-      // @ts-ignore
-      script.onreadystatechange = () => {
-        //alert("onreadystatechange " + this.readyState + " : " + (this.readyState === "complete" || this.readyState === "loaded"));
-        // @ts-ignore
-        if (this.readyState === "complete" || this.readyState === "loaded")
-          this.loadCallback(src);
-      };
-      script.onerror = function () {
-        delete Load.inProgress[Load.inProgress.indexOf(src)];
-        setTimeout(function () {
-          // self.ccLoad(src);
-        }, 2000);
-      };
-      Load.inProgress.push(src);
-      delete Load.depQueue[src];
-      document.getElementsByTagName('head')[0].appendChild(script);
-    } else {
-      Load.depQueue[src] = dependencies;
+    if (this.loadedJs.indexOf(file.src) >= 0) {
+      file.callback();
+    } else if (inProgressFile && inProgressFile.indexOf(file) === -1) {
+      inProgressFile.push(file);
+    } else if (!inProgressFile && !this.hasLoadedDependencies(file) && !alreadyInDepQueue) {
+      this.dependentFiles.push(file);
+      file.dependencies.forEach(dependencyFile => Load.loadJsOnce(dependencyFile));
+    } else if (!inProgressFile && this.hasLoadedDependencies(file)) {
+      this.load(file);
     }
   }
 
-  private static loadCallback(src: string) {
+  private static load(file: DependentFile) {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = file.src;
+    script.onload = () => this.onFileLoad(file.src);
+    // @ts-ignore
+    script.onreadystatechange = () => {
+      //alert("onreadystatechange " + this.readyState + " : " + (this.readyState === "complete" || this.readyState === "loaded"));
+      // @ts-ignore
+      if (this.readyState === "complete" || this.readyState === "loaded")
+        this.onFileLoad(file.src);
+    };
+    script.onerror = function () {
+      delete Load.inProgressFiles[file.src];
+      setTimeout(function () {
+        // self.ccLoad(src);
+      }, 2000);
+    };
+    if (!Load.inProgressFiles[file.src]) {
+      Load.inProgressFiles[file.src] = [];
+    }
+    Load.inProgressFiles[file.src].push(file);
+    Load.dependentFiles.splice(Load.dependentFiles.indexOf(file), 1);
+    document.getElementsByTagName('head')[0].appendChild(script);
+  }
+
+  private static onFileLoad(src: string) {
     //console.log("load callback " + src);
     Load.loadedJs.push(src);
-    delete Load.inProgress[Load.inProgress.indexOf(src)];
-    if (typeof Load.callback[src] === "function") {
-      Load.callback[src]();
-      delete Load.callback[src];
-    }
-    for (const scriptSrc in Load.depQueue) {
-      this.loadJsOnce(scriptSrc, null, Load.depQueue[scriptSrc]);
-    }
+    const files = Load.inProgressFiles[src];
+    delete Load.inProgressFiles[src];
+    files.filter(file => typeof file.callback === "function").forEach(
+      file => file.callback()
+    );
+    Load.dependentFiles.forEach(file => Load.loadJsOnce(file));
   }
 
-  private static hasLoadedDependencies(dependencies: string[]) {
-    const missingDependency = dependencies.find(dependency => Load.loadedJs.indexOf(dependency) === -1);
+  private static hasLoadedDependencies(file: DependentFile) {
+    const missingDependency = file.dependencies.find(dependency => Load.loadedJs.indexOf(dependency.src) === -1);
     return !missingDependency;
   }
 }
 
+class DependentFile {
+  constructor(
+    public src: string,
+    public callback: Function = function () {},
+    public dependencies: DependentFile[] = []
+  ) {
+  }
+}
