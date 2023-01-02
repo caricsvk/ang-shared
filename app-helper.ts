8ce4ea5e6a823587a02d9f9c89f3287c3da6e9e5
Richard Casar <caricsvk@gmail.com>
2023-01-02 11:57:40 -0800

    add copy to clipboard

--- app-helper.ts
+++ app-helper.ts
@@ -5,6 +5,41 @@ export class AppHelper {
     return currentHost.indexOf(domain) >= 0;
   }
 
+  public static copyTextToClipboard(text: string) {
+    if (!navigator.clipboard) {
+      this.fallbackCopyTextToClipboard(text);
+      return;
+    }
+    navigator.clipboard.writeText(text).then(function() {
+      console.log('Async: Copying to clipboard was successful!');
+    }, function(err) {
+      console.error('Async: Could not copy text: ', err);
+    });
+  }
+
+  private static fallbackCopyTextToClipboard(text: string) {
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+      var successful = document.execCommand('copy');
+      var msg = successful ? 'successful' : 'unsuccessful';
+      console.log('Fallback: Copying text command was ' + msg);
+    } catch (err) {
+      console.error('Fallback: Oops, unable to copy', err);
+    }
+
+    document.body.removeChild(textArea);
+  }
 }
 
 export const stringToEnum = <ET, T>(enumObj: ET, str: string): T =>
